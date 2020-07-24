7a5adebc3c21b6ed0947c7c6f032145647fa77bf
yugcodes <68748206+yugcodes@users.noreply.github.com>
2020-07-24 22:12:55 +0530

    added even and odd programs

---
+++ even-number.py
@@ -0,0 +1,5 @@
+#This is the program to print even numbers in python
+for i in range(1,25):
+    print(i)
+    if(i % 2 == 0):
+        print(i)
---
+++ odd-numbers.py
@@ -0,0 +1,4 @@
+# This is the program to print odd number in python
+for i in range(1,30):
+    if(i % 2 != 0):
+        print(i)
